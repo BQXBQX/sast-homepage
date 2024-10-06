b1eee713c473e163070c968be37eb97056f27832
bqxbqx <boqingxin14@gmail.com>
2024-10-06 15:21:49 +0800

    feat ✨: change welcome style

--- components/home/welcome/index.tsx
+++ components/home/welcome/index.tsx
@@ -7,7 +7,7 @@ import BulbPic from "@/public/img/bulb.png";
 import styles from "./index.module.scss";
 import { useTranslation } from "@/lib/i18n/server";
 import { LngProps } from "@/types/lng";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, MousePointerClick } from "lucide-react";
 
 interface WelcomeProps extends LngProps {}
 
@@ -15,22 +15,84 @@ export const Welcome: React.FC<WelcomeProps> = async ({ lng }) => {
   const { t } = await useTranslation(lng, "welcome");
 
   return (
-    <div className={styles["welcome-wrapper"]}>
-      <div className={styles["span-wrapper"]}>
-        <h1 className={`${styles.title}`}> {t("title")}</h1>
-        <h2 className={`${styles.desc}`}>Igniting the thought.</h2>
-        <Button className={styles.button}>
-          <span>{t("button")}</span>
-          <ArrowRight className={styles.svg} />
-        </Button>
-      </div>
-      <div className={styles.divider}></div>
-      <div className={styles["img-wrapper"]}>
-        <UnoptimizedImage
-          src={BulbPic}
-          alt="bulb"
-          style={{ width: "40%", objectFit: "contain" }}
-        />
+    <div style={{ width: "100%", height: "fit-content", padding: "0 12rem" }}>
+      <div className={styles["welcome-wrapper"]}>
+        <div className={styles["span-wrapper"]}>
+          <h1 className={`${styles.title}`}>
+            {t("title")}{" "}
+            <span className={styles["look"]}>
+              {t("look")}
+              <div
+                style={{
+                  position: "absolute",
+                  backgroundColor: "#dedede",
+                  height: "1rem",
+                  width: "1rem",
+                  left: "-0.5rem",
+                  top: "-0.5rem",
+                  borderRadius: "50%",
+                }}
+              />
+              <div
+                style={{
+                  position: "absolute",
+                  backgroundColor: "#dedede",
+                  height: "1rem",
+                  width: "1rem",
+                  right: "-0.5rem",
+                  top: "-0.5rem",
+                  borderRadius: "50%",
+                }}
+              />
+              <div
+                style={{
+                  position: "absolute",
+                  backgroundColor: "#dedede",
+                  height: "1rem",
+                  width: "1rem",
+                  left: "-0.5rem",
+                  bottom: "-0.5rem",
+                  borderRadius: "50%",
+                }}
+              />
+              <div
+                style={{
+                  position: "absolute",
+                  backgroundColor: "#dedede",
+                  height: "1rem",
+                  width: "1rem",
+                  right: "-0.5rem",
+                  bottom: "-0.5rem",
+                  borderRadius: "50%",
+                }}
+              />
+
+              <MousePointerClick
+                style={{
+                  position: "absolute",
+                  bottom: "-1.6rem",
+                  zIndex: "100",
+                  right: "2rem",
+                  height: "3rem",
+                  width: "3rem",
+                }}
+              />
+            </span>
+          </h1>
+          <h2 className={`${styles.desc}`}>Igniting the thought.</h2>
+          <Button className={styles.button}>
+            <span>{t("button")}</span>
+            <ArrowRight className={styles.svg} />
+          </Button>
+        </div>
+        <div className={styles.divider}></div>
+        <div className={styles["img-wrapper"]}>
+          <UnoptimizedImage
+            src={BulbPic}
+            alt="bulb"
+            style={{ width: "40%", objectFit: "contain" }}
+          />
+        </div>
       </div>
     </div>
   );
